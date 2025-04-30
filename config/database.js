87e855cd87a8dad32d1ce5a386b5b496fb0a30fb
Raghuveer Chauhan <raghuveerchauhan7787@gmail.com>
2025-05-01 00:04:46 +0530

    adding database to atlas

--- config/database.js
+++ config/database.js
@@ -1,12 +1,18 @@
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
 
-const connectDB=async()=>{
-    try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/Live-Editor");
-        console.log("database successfully connected");
-    } catch (error) {
-        console.log("Error in database connection :",error);
-    }
-}
+const connectDB = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://raghuveerchauhan7787:q5DRcxyO954WNNqb@cluster0.kmiue.mongodb.net/code-editor?retryWrites=true&w=majority&appName=Cluster0"
+    );
+    console.log("database successfully connected to atlas");
+  } catch (error) {
+    console.log("Error in database connection :", error);
+  }
+};
 
-module.exports= connectDB;
+module.exports = connectDB;
+
+// "mongodb+srv://raghuveerchauhan7787:q5DRcxyO954WNNqb@cluster0.kmiue.mongodb.net/code-editor?retryWrites=true&w=majority&appName=Cluster0"
+
+// "mongodb://127.0.0.1:27017/Live-Editor"
